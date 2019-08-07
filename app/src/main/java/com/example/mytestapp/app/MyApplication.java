e0dcbb1611928d8466f3b3c30dce4bf3bba0f113
wangbufan <wangbufan0@163.com>
2019-08-07 17:03:14 +0800

    feat:add OSS

---
+++ app/src/main/java/com/example/mytestapp/app/MyApplication.java
@@ -0,0 +1,20 @@
+package com.example.mytestapp.app;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+
+
+
+    private static Context context=null;
+    public static Context getContext(){
+        return context;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context=getApplicationContext();
+    }
+}
---
+++ app/src/main/java/com/example/mytestapp/upload/UploadHelper.java
@@ -0,0 +1,128 @@
+package com.example.mytestapp.upload;
+
+import android.text.format.DateFormat;
+
+import com.alibaba.sdk.android.oss.OSS;
+import com.alibaba.sdk.android.oss.OSSClient;
+import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider;
+import com.alibaba.sdk.android.oss.common.auth.OSSPlainTextAKSKCredentialProvider;
+import com.alibaba.sdk.android.oss.model.PutObjectRequest;
+import com.alibaba.sdk.android.oss.model.PutObjectResult;
+import com.example.mytestapp.app.MyApplication;
+import com.example.mytestapp.utils.HashUtil;
+
+import java.io.File;
+import java.util.Date;
+
+public class UploadHelper {
+
+    //与个人的存储区域有关
+    private static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
+    //上传仓库名
+    private static final String BUCKET_NAME = "wangbufan0";
+
+    private static OSS getOSSClient() {
+        OSSCredentialProvider credentialProvider =
+                new OSSPlainTextAKSKCredentialProvider("LTAIu1QgPcjiPpfU" ,
+                        "fSCJzvogE20n7nx4iuR1v7k5BB8jGW");
+
+        return new OSSClient( MyApplication.getContext(), ENDPOINT, credentialProvider);
+    }
+
+    /**
+     * 上传方法
+     *
+     * @param objectKey 标识
+     * @param path      需上传文件的路径
+     * @return 外网访问的路径
+     */
+    private static String upload(String objectKey, String path) {
+        // 构造上传请求
+        PutObjectRequest request =
+                new PutObjectRequest(BUCKET_NAME,
+                        objectKey, path);
+        try {
+            //得到client
+            OSS client = getOSSClient();
+            //上传获取结果
+            PutObjectResult result = client.putObject(request);
+            //获取可访问的url
+            String url = client.presignPublicObjectURL(BUCKET_NAME, objectKey);
+            return url;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     * 上传普通图片
+     *
+     * @param path 本地地址
+     * @return 服务器地址
+     */
+    public static String uploadImage(String path) {
+        String key = getObjectImageKey(path);
+        return upload(key, path);
+    }
+
+    /**
+     * 上传头像
+     *
+     * @param path 本地地址
+     * @return 服务器地址
+     */
+    public static String uploadPortrait(String path) {
+        String key = getObjectPortraitKey(path);
+        return upload(key, path);
+    }
+
+    /**
+     * 上传audio
+     *
+     * @param path 本地地址
+     * @return 服务器地址
+     */
+    public static String uploadAudio(String path) {
+        String key = getObjectAudioKey(path);
+        return upload(key, path);
+    }
+
+
+    /**
+     * 获取时间
+     *
+     * @return 时间戳 例如:201805
+     */
+    private static String getDateString() {
+        return DateFormat.format("yyyyMM", new Date()).toString();
+    }
+
+    /**
+     * 返回key
+     *
+     * @param path 本地路径
+     * @return key
+     */
+    //格式: image/201805/sfdsgfsdvsdfdsfs.jpg
+    private static String getObjectImageKey(String path) {
+        String fileMd5 = HashUtil.getMD5String(new File(path));
+        String dateString = getDateString();
+        return String.format("image/%s/%s.jpg", dateString, fileMd5);
+    }
+
+    //格式: portrait/201805/sfdsgfsdvsdfdsfs.jpg
+    private static String getObjectPortraitKey(String path) {
+        String fileMd5 = HashUtil.getMD5String(new File(path));
+        String dateString = getDateString();
+        return String.format("portrait/%s/%s.jpg", dateString, fileMd5);
+    }
+
+    //格式: audio/201805/sfdsgfsdvsdfdsfs.mp3
+    private static String getObjectAudioKey(String path) {
+        String fileMd5 = HashUtil.getMD5String(new File(path));
+        String dateString = getDateString();
+        return String.format("audio/%s/%s.mp3", dateString, fileMd5);
+    }
+}
